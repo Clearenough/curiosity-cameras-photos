4adfab04c44c962c81386028f02dab1ee634620c
Clearenough <smogida1337@gmail.com>
2023-08-07 12:47:05 +0300

    feat: add header for Photos screen

--- src/screens/Photos.jsx
+++ src/screens/Photos.jsx
@@ -1,17 +1,64 @@
-import { SafeAreaView, Text, Pressable } from 'react-native'
+import { SafeAreaView, Text, Pressable, View, StyleSheet } from 'react-native'
 import BackSvg from '../components/Svg/BackSvg'
+import { accordance } from '../constants/constants'
+import { dateTransform } from '../helpers/dateTransform'
+import { useFonts } from 'expo-font'
 
 function PhotosScreen({ route, navigation }) {
+  const [fontsLoaded] = useFonts({
+    'Terminal-Dosis-Regular': require('../../assets/fonts/TerminalDosis-Regular.ttf'),
+    'Dosis-SemiBold': require('../../assets/fonts/Dosis-SemiBold.ttf')
+  })
+
   const { photos, camera, date } = route.params
 
   return (
-    <SafeAreaView>
-      <Text>{camera}</Text>
-      <Pressable onPress={() => navigation.goBack()}>
-        <BackSvg />
-      </Pressable>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.icon}>
+          <Pressable onPress={() => navigation.goBack()}>
+            <BackSvg />
+          </Pressable>
+        </View>
+        <View style={styles.info}>
+          <Text style={styles.camera}>{accordance[camera]}</Text>
+          <Text style={styles.date}>{dateTransform(date)}</Text>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 42
+  },
+  icon: {
+    position: 'absolute',
+    left: 16,
+    width: 24,
+    height: 24
+  },
+  info: {
+    alignSelf: 'center',
+    alignItems: 'center'
+  },
+  camera: {
+    fontFamily: 'Dosis-SemiBold',
+    fontSize: 18,
+    lineHeight: 22
+  },
+  date: {
+    fontFamily: 'Terminal-Dosis-Regular',
+    fontSize: 13,
+    lineHeight: 22
+  }
+})
 
 export default PhotosScreen
